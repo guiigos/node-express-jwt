c158fd2ec6fbbe2a937959d827aa4ec74aa85bd2
Guilherme Alves <guiigos.alves@gmail.com>
2018-05-17 18:55:49 -0300

    Atualização

--- README.md
+++ README.md
@@ -1,26 +1,30 @@
 # NodeJs Token
 > Sample project of using **token authentication** using [jsonwebtoken](https://github.com/auth0/node-jsonwebtoken).
+> - [Overview](#overview)
 > - [Usage](#usage)
 >   - [Environment](#environment)
 >   - [Dependencies](#dependencies)
 >   - [Run](#run)
 > - [License](#license)
 
+## Overview
+Example of using token authentication in NodeJs projects. The use of tokens as an additional authentication mechanism has become increasingly common in the marketplace and provides the highest level of security. Authenticated routes in the application can only be accessed using access token.
+
 ## Usage
 ### Environment
-Installation
+Commands to install the development environment.
 ```bash
 $ npm install npm -g
 ```
 
 ### Dependencies
-Install dependencies and modules
+Install dependencies and modules.
 ```bash
 $ npm install
 ```
 
 ### Run
-Compile project
+After installing the dependencies the application is ready to run.
 ```bash
 $ npm start
 $ npm run nodemon
--- index.js
+++ index.js
@@ -9,33 +9,33 @@ const express = require('express');
 var app = express();
 
 var cors = function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
 }
 
 // autenticação utilizando somente o jwt
 var _jwt = function (req, res, next) {
-    var token = req.body.token || req.param('token') || req.headers['x-access-token'];
-    if (token) {
-        jwt.verify(token, app.get('tokenSecret'), (err, decoded) => {
-            if (err) return res.json({ success: false, message: 'Não foi possivel autenticar.' });
-            else {
-                req.decoded = decoded;
-                next();
-            }
-        });
-    } else {
-        return res.status(403).send({
-            success: false,
-            message: 'Nenhum token informado.'
-        });
-    }
+  var token = req.body.token || req.param('token') || req.headers['x-access-token'];
+  if (token) {
+    jwt.verify(token, app.get('tokenSecret'), (error, decoded) => {
+      if (error) return res.json({ success: false, message: 'Não foi possivel autenticar.', });
+      else {
+        req.decoded = decoded;
+        next();
+      }
+    });
+  } else {
+    return res.status(403).send({
+      success: false,
+      message: 'Nenhum token informado.',
+    });
+  }
 };
 
 app.use(cors);
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false, }));
 app.use(bodyParser.json());
 app.use(expressValidator());
 app.use(morgan('dev'));
@@ -44,49 +44,49 @@ app.set('tokenSecret', 'guilherme@alves');
 app.set('usuario', 'guilherme');
 app.set('senha', '123456');
 
-app.get('/', function (req, res) {
-    res.send(`
-        <h1>Exemplo de autenticação via token</h1>
+app.get('/', function (req, res, next) {
+  res.send(`
+    <h1>Exemplo de autenticação via token</h1>
+    <br>
+    <ul>
+      <li>
+        <p><strong>POST</strong> autenticação rota <strong><a href="http://localhost:${port}/authenticate">Authenticate</a></strong>
         <br>
-        <ul>
-            <li>
-                <p><strong>POST</strong> autenticação rota <strong><a href="http://localhost:${port}/authenticate">Authenticate</a></strong>
-                <br>
-                <strong>Usuário:</strong> ${app.get('usuario')} <strong>Senha:</strong> ${app.get('senha')}</p>
-            </li>
-            <li>
-                <p><strong>GET</strong> rotas autenticadas <strong><a href="http://localhost:${port}/api">API</a></strong></p>
-            </li>
-        </ul>
-    `);
+        <strong>Usuário:</strong> ${app.get('usuario')} <strong>Senha:</strong> ${app.get('senha')}</p>
+      </li>
+      <li>
+        <p><strong>GET</strong> rotas autenticadas <strong><a href="http://localhost:${port}/api">API</a></strong></p>
+      </li>
+    </ul>
+  `);
 });
 
-app.post('/authenticate', function (req, res) {
-    req.assert('user', 'Usuário é obrigatório.').notEmpty();
-    req.assert('password', 'Senha é obrigatória.').notEmpty();
+app.post('/authenticate', function (req, res, next) {
+  req.assert('user', 'Usuário é obrigatório.').notEmpty();
+  req.assert('password', 'Senha é obrigatória.').notEmpty();
 
-    var erros = req.validationErrors();
-    if (erros) res.json(erros);
-    else {
-        if (req.body.user == app.get('usuario') && req.body.password == app.get('senha')) {
+  var erros = req.validationErrors();
+  if (erros) res.json(erros);
+  else {
+    if (req.body.user == app.get('usuario') && req.body.password == app.get('senha')) {
 
-            let payload = {
-                'usuario': req.body.user,
-                'password': req.body.password
-            };
+      let payload = {
+        'usuario': req.body.user,
+        'password': req.body.password,
+      };
 
-            let options = {
-                expiresIn: 86400 // 24 horas
-            };
+      let options = {
+        expiresIn: 86400, // 24 horas
+      };
 
-            jwt.sign(payload, app.get('tokenSecret'), options, (token) => {
-                res.json({
-                    'token': token,
-                    'usuario': req.body.user
-                });
-            });
-        } else res.json({ success: false, message: 'Não foi possível autenticar.' });
-    }
+      jwt.sign(payload, app.get('tokenSecret'), options, (token) => {
+        res.json({
+          'token': token,
+          'usuario': req.body.user,
+        });
+      });
+    } else res.json({ success: false, message: 'Não foi possível autenticar.', });
+  }
 });
 
 // rotas autenticadas diretamente pelo _jwt
@@ -94,8 +94,8 @@ var apiRoutes = express.Router();
 
 apiRoutes.use(_jwt);
 
-apiRoutes.get('/', function (req, res) {
-    res.json({ message: 'Bem vindo!' });
+apiRoutes.get('/', function (req, res, next) {
+  res.json({ message: 'Bem vindo!', });
 });
 
 app.use('/api', apiRoutes);
